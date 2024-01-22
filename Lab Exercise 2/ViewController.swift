aeca63ba7895133244ea930c902bac5bcca7c035
AZAD <mandariazad6666@gmail.com>
2024-01-22 10:10:14 -0800

    Initial Commit

---
+++ Lab Exercise 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab Exercise 2
+//
+//  Created by user240113 on 1/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
