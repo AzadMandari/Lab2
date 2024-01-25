fb02c9efd763001b4cbd85ee9d336bfd6b332a9b
AZAD <mandariazad6666@gmail.com>
2024-01-24 17:51:20 -0800

    Ajji commit

--- Lab Exercise 2/ViewController.swift
+++ Lab Exercise 2/ViewController.swift
@@ -8,7 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelValue: UILabel!
+    var tempNumberHolder = 0
+    var step = 1
+    
+    @IBAction func buttondec(_ sender: Any) {
+        tempNumberHolder -= step
+        labelValue.text = String(tempNumberHolder)
+    }
+    
+    @IBAction func buttoninc(_ sender: Any) {
+        tempNumberHolder += step
+        labelValue.text = String(tempNumberHolder)
+        
+    }
+    
+    
+   
+    @IBAction func buttonReset(_ sender: Any) {
+        tempNumberHolder = 0
+        step = 1
+        labelValue.text = String(tempNumberHolder)
+    }
+    
+    
+    @IBAction func buttoninc2(_ sender: Any) {
+    
+        step = 2
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
